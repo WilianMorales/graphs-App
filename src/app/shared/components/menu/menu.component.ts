104b99a58e0bf16fcc12a1d89cf18e44d3c30629
Wilian Morales <willian.moralesch@gmail.com>
2025-02-26 02:09:34 -0500

    Implementation of the menuComponent and its styles.

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    ` li { cursor: pointer; }`
   ]
 })
 export class MenuComponent {
+
+  public menu: MenuItem[] = [
+    { path: '/graphics/bar-chart', title: 'Diagrama de Barras' },
+    { path: '/graphics/double-chart', title: 'Doble Gráfica' },
+    { path: '/graphics/doughnut-chart', title: 'Diagrama de Dona' },
+    { path: '/graphics/doughnut-chart-http', title: 'Diagrama de Dona - Http' }
+  ];
 
 }
